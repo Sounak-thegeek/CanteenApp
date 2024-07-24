00da0791987ee07a7d34fdae570ed80b8aab10ec
Sounak Banerjee <sounakjsr@gmail.com>
2024-07-24 18:16:09 +0530

    added the user interface of canteen management app

---
+++ src/validation/validators.js
@@ -0,0 +1,8 @@
+export const validateUserName = (userName) => {
+    return userName.includes(".com") && userName.includes("@");
+  };
+  
+  export const validateUserPassword = (password) => {
+    return password.length > 0 && password.length === 8;
+  };
+  
